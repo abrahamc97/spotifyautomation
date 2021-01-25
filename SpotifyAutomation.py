f5da65bfe84737f6f01ab14abacef7f4a1ceb16f
abrahamc97 <chris.a147@gmail.com>
2021-01-24 20:20:04 -0500

    Added message randomizer

--- SpotifyAutomation.py
+++ SpotifyAutomation.py
@@ -23,7 +23,7 @@ sys.stdout = open("History.txt", "a")
 today = datetime.now()
 print('New Entry')
 print(today)
-if today.day == 19:
+if today.day == 1:
     sp= createSpotifyEnv(spotify_user_id,spotify_token)
     name= makePlaylistName()
     print(name)
@@ -31,7 +31,9 @@ if today.day == 19:
     makeMonthlyPlaylist(sp,username,name,desc)
     monthlyurl= getMonthURL(sp, username, name)
     print(monthlyurl)
-    message_id=sendPlaylist(user_id, password, thread, "Here's this month's playlist: " + monthlyurl)
+    sample_texts = ["Please bless this playlist with some music: ", "Here you go humans: ", "No, it's not Christmas, but here's a playlist: ", "It's about that time again: ", "You know what to do: ", "I need some new groovy tunes: ", "I need 100cc of new music. Stat!: ", "My doctor said music should help my grumpiness. Help me out: ", "I'll just leave this here: ", "Hear ye. Hear ye. I require music now please: ", "If you don't add music, I'm telling on you: ", "Neethu's mom said, 'Add music, or else.' You better listen to her: ", "This following message is from John. 'Here's a scrumptious playlist for y'all': ", "President Biden said it's your constitutional duty to add music to the playlist, soooo...: ", "I don't have anything creative to say this month. Here's this month's playlist: ", "Some of you are deficient in Vitamin Do, Re, and Mi. So here you go: ", "Richu says 'Add music to the playlist your cowards.': ", ""]
+    text= random.choice(sample_texts)
+    message_id=sendPlaylist(user_id, password, thread, text + monthlyurl)
     print(message_id)
 d1= date.today()
 year= d1.year
